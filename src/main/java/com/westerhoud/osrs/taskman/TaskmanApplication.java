974f6cd75311cbe0ec2589087473d0b3d992d827
Remco Westerhoud <remcowesterhoud@msn.com>
2022-08-13 09:35:56 +0200

    small fix

--- src/main/java/com/westerhoud/osrs/taskman/TaskmanApplication.java
+++ src/main/java/com/westerhoud/osrs/taskman/TaskmanApplication.java
@@ -2,8 +2,9 @@ package com.westerhoud.osrs.taskman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TaskmanApplication {
 
 	public static void main(String[] args) {
