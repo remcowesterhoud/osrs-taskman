526efcebf70b17e59e3771668e6f4020fff2370f
Remco Westerhoud <remcowesterhoud@msn.com>
2022-08-13 09:53:45 +0200

    Hello world

--- src/main/java/com/westerhoud/osrs/taskman/TaskmanApplication.java
+++ src/main/java/com/westerhoud/osrs/taskman/TaskmanApplication.java
@@ -3,9 +3,19 @@ package com.westerhoud.osrs.taskman;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class TaskmanApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String helloWorld() {
+		return "Hello, world!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanApplication.class, args);
