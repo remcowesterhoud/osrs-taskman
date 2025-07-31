ba3635969f06dd07f5f14ea14b1c7e73c02b01d5
Remco Westerhoud <remcowesterhoud@camunda.com>
2025-07-31 18:53:56 +0200

    feat: attempt to reduce memory usage

--- src/main/java/com/westerhoud/osrs/taskman/TaskmanApplication.java
+++ src/main/java/com/westerhoud/osrs/taskman/TaskmanApplication.java
@@ -2,12 +2,33 @@ package com.westerhoud.osrs.taskman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
+import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.mustache.MustacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	DataSourceAutoConfiguration.class,
+	SqlInitializationAutoConfiguration.class,
+	MultipartAutoConfiguration.class,
+	WebSocketServletAutoConfiguration.class,
+	GsonAutoConfiguration.class,
+	AopAutoConfiguration.class
+})
 @EnableScheduling
 public class TaskmanApplication {
 
